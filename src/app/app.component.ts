c4fee8c0daacec7acb294a46cd43684faacb6a69
RalphyEsmeraldas <rrc.cabrera@yavirac.edu.ec>
2018-02-20 16:18:14 -0500

    Inicio del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <app-left-navigation></app-left-navigation>
+    <app-right-navigation></app-right-navigation>
     <app-header></app-header>
     <app-footer></app-footer>
   `,
